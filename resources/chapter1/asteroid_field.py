9ef07a86b69020257f92a3c6a04efcee3a8ab94b
James Capps <jcapps@protonmail.com>
2020-03-17 14:45:22 -0500

    Working on asteroid field.

--- resources/chapter1/asteroid_field.py
+++ resources/chapter1/asteroid_field.py
@@ -1,12 +1,24 @@
 def game():
 
     import turtle
-    import os
+    import math
+    import random
+
 
     # Set up the screen
     wn = turtle.Screen()
     wn.bgcolor('black')
     wn.title('T h e  A s t e r o i d  F i e l d')
+    wn.bgpic('img/asteroid_background.gif')
+
+    # Register the images
+    wn.register_shape('img/small_rocket.gif')
+    wn.register_shape('img/asteroid.gif')
+    wn.register_shape('img/laser.gif')
+    wn.register_shape('img/exhaust.gif')
+    wn.register_shape('img/control_panel.gif')
+    wn.register_shape('img/control_panel_right_side.gif')
+    wn.register_shape('img/control_panel_left_side.gif')
 
     # Draw border
     border_pen = turtle.Turtle()
@@ -22,31 +34,96 @@ def game():
 
     border_pen.hideturtle()
 
+    # Set score to zero
+    distance_to_end = 1000
+
+    # Draw the distance left
+    score_pen = turtle.Turtle()
+    score_pen.speed(0)
+    score_pen.color('white')
+    score_pen.penup()
+    score_pen.setposition(-450, 375)
+    score_string = f'Distance to end of asteroid field: {distance_to_end} m'
+    score_pen.write(score_string, False, align='left', font=('Monospace', 12, 'normal'))
+    score_pen.hideturtle()
+
+    # When lives left goes to zero game over.
+    lives_left = 3
+
+    # Draw lives left then remove one after rocket and asteroid collide.
+    # Life 1
+    rocket_lives_one = turtle.Turtle()
+    rocket_lives_one.shape('img/small_rocket.gif')
+    rocket_lives_one.penup()
+    rocket_lives_one.setposition(185, 335)
+    # Life 2
+    rocket_lives_two = turtle.Turtle()
+    rocket_lives_two.shape('img/small_rocket.gif')
+    rocket_lives_two.penup()
+    rocket_lives_two.setposition(230, 335)
+    # Life 3
+    rocket_lives_three = turtle.Turtle()
+    rocket_lives_three.shape('img/small_rocket.gif')
+    rocket_lives_three.penup()
+    rocket_lives_three.setposition(275, 335)
+
+    # Create the control panel
+    control_panel = turtle.Turtle()
+    control_panel.shape('img/control_panel.gif')
+    control_panel.penup()
+    control_panel.setposition(0, -350)
+    control_panel_right_side = turtle.Turtle()
+    control_panel_right_side.shape('img/control_panel_right_side.gif')
+    control_panel_right_side.penup()
+    control_panel_right_side.setposition(200, -350)
+    control_panel_left_side = turtle.Turtle()
+    control_panel_left_side.shape('img/control_panel_left_side.gif')
+    control_panel_left_side.penup()
+    control_panel_left_side.setposition(-200, -350)
+
     # Create the player turtle
     player = turtle.Turtle()
-    player.color('blue')
-    player.shape('triangle')
+    player.shape('img/small_rocket.gif')
     player.penup()
     player.speed(0)
     player.setposition(0, -250)
     player.setheading(90)
 
+    # Create the exhaust turtle
+    exhaust = turtle.Turtle()
+    exhaust.shape('img/exhaust.gif')
+    exhaust.penup()
+    exhaust.speed(0)
+    exhaust.setposition(0, -325)
+    exhaust.setheading(90)
+    exhaust.hideturtle()
+
     player_speed = 10
 
-    # Create the enemy
-    enemy = turtle.Turtle()
-    enemy.color('red')
-    enemy.shape('circle')
-    enemy.penup()
-    enemy.speed(0)
-    enemy.setposition(-200, 250)
+    # Choose number of enemies
+    number_of_enemies = 5
+    # Create an empty list of enemies
+    enemies = []
+    # Add enemies to the list
+    for i in range(number_of_enemies):
+        enemies.append(turtle.Turtle())
+
+    for enemy in enemies:
+        # Create the enemy
+        enemy.color('red')
+        enemy.shape('img/asteroid.gif')
+        enemy.penup()
+        enemy.speed(0)
+        x = random.randint(-200, 200)
+        y = random.randint(100, 250)
+        enemy.setposition(x, y)
 
     enemy_speed = 3
 
     # Create the weapon
     weapon = turtle.Turtle()
-    weapon.color('yellow')
-    weapon.shape('triangle')
+    weapon.color('blue')
+    weapon.shape('img/laser.gif')
     weapon.penup()
     weapon.speed(0)
     weapon.setheading(90)
@@ -55,13 +132,14 @@ def game():
 
     weapon_speed = 20
 
-    # Move ship
+    # Functions
     def move_left():
         x = player.xcor()
         x -= player_speed
         if x < -280:
             x = -280
         player.setx(x)
+        exhaust.setx(x)
 
     def move_right():
         x = player.xcor()
@@ -69,6 +147,24 @@ def game():
         if x > 280:
             x = 280
         player.setx(x)
+        exhaust.setx(x)
+
+    def move_up():
+        y = player.ycor()
+        y += player_speed
+        if y > 265:
+            y = 265
+        player.sety(y)
+        exhaust.sety(y - 75)
+        exhaust.showturtle()
+        exhaust.hideturtle()
+
+    def move_down():
+        y = player.ycor()
+        y -= player_speed
+        if y < -265:
+            y = -265
+        player.sety(y)
 
     def fire_weapon():
         global weapon_state
@@ -81,31 +177,73 @@ def game():
             weapon.setposition(x, y)
             weapon.showturtle()
 
+    def is_collision(t1, t2):
+        distance = math.sqrt(math.pow(t1.xcor() - t2.xcor(), 2) + math.pow(t1.ycor() - t2.ycor(), 2))
+        if distance < 20:
+            return True
+        else:
+            return False
+
     # Key binding
     wn.listen()
     wn.onkeypress(move_left, 'Left')
     wn.onkeypress(move_right, 'Right')
+    wn.onkeypress(move_up, 'Up')
+    wn.onkeypress(move_down, 'Down')
     wn.onkeypress(fire_weapon, 'space')
 
     # Main game loop
     while True:
-        # Move the enemy
-        x = enemy.xcor()
-        x += enemy_speed
-        enemy.setx(x)
 
-        # Boundary check, then drop.
-        if enemy.xcor() > 280:
-            y = enemy.ycor()
-            y -= 40
-            enemy_speed *= -1
-            enemy.sety(y)
+        for enemy in enemies:
+            # Move the enemy
+            x = enemy.xcor()
+            x += enemy_speed
+            enemy.setx(x)
 
-        if enemy.xcor() < -280:
-            y = enemy.ycor()
-            y -= 40
-            enemy_speed *= -1
-            enemy.sety(y)
+            # Boundary check, then drop.
+            if enemy.xcor() > 280:
+                for e in enemies:
+                    y = e.ycor()
+                    y -= 40
+                    e.sety(y)
+                enemy_speed *= -1
+
+            if enemy.xcor() < -280:
+                for e in enemies:
+                    y = e.ycor()
+                    y -= 40
+                    e.sety(y)
+                enemy_speed *= -1
+
+            # Check for laser collision.
+            if is_collision(weapon, enemy):
+                # Reset the weapon
+                weapon.hideturtle()
+                weapon_state = 'ready'
+                weapon.setposition(0, -400)
+                # Reset the enemy
+                x = random.randint(-200, 200)
+                y = random.randint(100, 250)
+                enemy.setposition(x, y)
+                #Update the score
+                #score += 10
+                #score_string = f'Score: {score}'
+                #score_pen.clear()
+                #score_pen.write(score_string, False, align='left', font=('Arial', 14, 'normal'))
+
+            # Check for collision between player and enemy
+            if is_collision(player, enemy):
+                player.hideturtle()
+                enemy.hideturtle()
+                lives_left -= 1
+                if lives_left == 2:
+                    rocket_lives_one.hideturtle()
+                if lives_left == 1:
+                    rocket_lives_two.hideturtle()
+                if lives_left == 0:
+                    print('Game Over')
+                # Need to restart here...
 
         # Move laser
         y = weapon.ycor()
@@ -115,6 +253,8 @@ def game():
         if weapon.ycor() > 275:
             weapon.hideturtle()
             weapon_state = 'ready'
+
+
 
 
 
